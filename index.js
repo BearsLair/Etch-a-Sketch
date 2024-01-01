a3984af0378a905dc3ee97d0f2c2ce33bb0026a9
Patrick Gross <bears_lair@protonmail.com>
2024-01-01 15:29:48 -0800

    Colors squares on mouse move

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const container = document.querySelector("div.container");
 
 for (let i = 1; i <= 256; i++) {
   const square = document.createElement("div");
+
   container.appendChild(square);
+}
+
+let newColor = document.querySelectorAll(".container > div");
+
+for (const color of newColor) {
+  color.addEventListener("mousedown", () => {
+    color.style.backgroundColor = "black";
+  });
 }
