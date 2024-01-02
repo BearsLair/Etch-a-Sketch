bb630fe3ba3d692e7ec0bfe122205535149a8d3e
Patrick Gross <bears_lair@protonmail.com>
2024-01-01 23:58:12 -0800

    reworking grid creation

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const container = document.querySelector("div.container");
 
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= 16; i++) {
   const square = document.createElement("div");
-
+  square.classList.add("row");
   container.appendChild(square);
+  for (let j = 1; j <= 16 - 1; j++) {
+    const square = document.createElement("div");
+    square.classList.add("column");
+    container.appendChild(square);
+  }
 }
 
-let newColor = document.querySelectorAll(".container > div");
+// let newColor = document.querySelectorAll(".container > div");
 
-for (const color of newColor) {
-  color.addEventListener("mousedown", () => {
-    color.style.backgroundColor = "black";
-  });
-}
+// // for (const color of newColor) {
+// //   color.addEventListener("mouseover", () => {
+// //     color.style.backgroundColor = "black";
+// //   });
+// // };
