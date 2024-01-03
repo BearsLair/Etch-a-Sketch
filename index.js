6ce1eddc4939756b4f36dfb37f28c3b212fee77d
Patrick Gross <bears_lair@protonmail.com>
2024-01-02 20:40:03 -0800

    Basic Etch-a-Sketch in place. Need user input limits.

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const container = document.querySelector("div.container");
 
-let userInput = 100;
+let row;
 
-for (let i = 1; i <= userInput; i++) {
-  console.log(userInput);
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
-  for (let j = 1; j <= userInput; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.height = (1 / userInput) * 320 + "px";
-    square.style.width = (1 / userInput) * 320 + "px";
-    row.appendChild(square);
+let userInput = 16;
+drawGrid(userInput);
+applyColor();
+
+function drawGrid(userInput) {
+  for (let i = 1; i <= userInput; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 1; j <= userInput; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.height = (1 / userInput) * 500 + "px";
+      square.style.width = (1 / userInput) * 500 + "px";
+      row.appendChild(square);
+    }
   }
 }
 
-let newColor = document.querySelectorAll(".square");
+function applyColor() {
+  let newColor = document.querySelectorAll(".square");
 
-for (const color of newColor) {
-  color.addEventListener("mouseover", () => {
-    color.style.backgroundColor = "black";
-  });
+  for (const color of newColor) {
+    color.addEventListener("mouseover", () => {
+      color.style.backgroundColor = "black";
+    });
+  }
 }
+
+size = document.querySelector("#sizeBtn");
+
+size.addEventListener("click", () => {
+  let newSize = document.querySelector("#userInput").value;
+  let userInput = newSize;
+
+  document.querySelectorAll(".row").forEach((row) => row.remove());
+
+  console.log(userInput);
+
+  drawGrid(userInput);
+  applyColor();
+});
