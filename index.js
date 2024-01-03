b3d70658cd42399d3a43f5d45c0257f8585efa0c
Patrick Gross <bears_lair@protonmail.com>
2024-01-02 17:20:15 -0800

    Javascript 16x16 grid created. Can't do larger sizes.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const container = document.querySelector("div.container");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 32; i++) {
   const row = document.createElement("div");
   row.classList.add("row");
   container.appendChild(row);
-  for (let j = 1; j <= 16; j++) {
+  for (let j = 1; j <= 32; j++) {
     const square = document.createElement("div");
     square.classList.add("square");
     row.appendChild(square);
   }
 }
 
-// let newColor = document.querySelectorAll(".container > div");
+let newColor = document.querySelectorAll(".square");
 
-// // for (const color of newColor) {
-// //   color.addEventListener("mouseover", () => {
-// //     color.style.backgroundColor = "black";
-// //   });
-// // };
+for (const color of newColor) {
+  color.addEventListener("mouseover", () => {
+    color.style.backgroundColor = "black";
+  });
+}
